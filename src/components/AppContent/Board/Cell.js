6dfac3aa154994a9f9d9cecb043970eafdaa28d0
vividsid94 <siddharth.murali2008@gmail.com>
2022-12-22 07:29:14 -0600

    general

--- src/components/AppContent/Board/Cell.js
+++ src/components/AppContent/Board/Cell.js
@@ -1,36 +1,15 @@
 import {useState, useEffect} from 'react';
+import styles from './Cell.module.css';
+
 export default function Cell(rowIndex, colIndex, bonus, type) {
-    function displayTiles(word, place){
-        for (var i = 0; i < String(word).length; i++) {
-            var r = document.getElementsByClassName(place);
-            var source = "./images/white-pro/";
-            source += word[i];
-            source += ".png"
-            r[i].src = source;
-            if (word.length == 7){
-                r[7].src = "";
-                r[i].style.width = "10%";
-            }
-            if (word.length == 8){
-                r[i].style.width = "8%";
-            }
-        }
-    }
-
-
-
-
-    if (type === "board"){
-        //return (<img style={{ width: '20px', height: '20px'}} src="images/white-pro/A.png"></img>);
-        return (<div style={{ backgroundColor: bonus.color, backgroundImage: bonus.image, display: 'flex', width: '30px', height: '30px', textAlign: 'center', justifyContent: 'center', alignItems: 'flex-end', fontSize: '25px' }}>{bonus.value}</div>);
-    }
-    else if (type === "pool"){
-        return (<div style={{ backgroundColor: "lightgrey", display: 'flex', width: '15px', height: '15px', textAlign: 'center', justifyContent: 'center', alignItems: 'center', border: "2px solid black", color: "black", fontSize: '12px' }}>{bonus.value}</div>);
-    }
-    else if (type === "rack"){
-        return bonus.value;
-    }
-    else{
-        return (<div style={{ backgroundColor: bonus.color, display: 'flex', width: '40px', height: '40px', textAlign: 'center', justifyContent: 'center', alignItems: 'flex-end' }}>{bonus.value}</div>);
+    switch (type) {
+        case "board":
+            return (<div className={styles.cellBoard} style={{ backgroundColor: bonus.color, backgroundImage: bonus.image}}>{bonus.value}</div>);
+        case "pool":
+            return (<div className={styles.cellPool}>{bonus.value}</div>);
+        case "rack":
+            return bonus.value;
+        default:
+            return bonus.value;
     }
 }
--- src/components/AppContent/Board/Pool.js
+++ src/components/AppContent/Board/Pool.js
@@ -3,26 +3,31 @@ import cellBonusMap from './cellType';
 import styles from './Pool.module.css';
 
 export default function Pool(props) {
-    // Create a new 2D array with 7 elements per inner array
+  const rows = [];
+  let currentRow = [];
 
-    const rows = [];
-    for (let i = 0; i < props.board.length; i += 14) {
-      rows.push(props.board.slice(i, i + 14));
+  for (let i = 0; i < props.board.length; i++) {
+    currentRow.push(props.board[i]);
+
+    if (currentRow.length === 14 || i === props.board.length - 1) {
+      rows.push(currentRow);
+      currentRow = [];
     }
-  
-    return (
-      <div className={styles.poolTbl}>
-        <table>
-          <tbody>
-            {rows.map((row, rowIndex) => (
-              <tr key={rowIndex}>
-                {row.map((col, colIndex) => (
-                  <td key={colIndex}>{col}</td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
   }
+
+  return (
+    <div className={styles.poolTbl}>
+      <table>
+        <tbody>
+          {rows.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((col, colIndex) => (
+                <td key={colIndex}>{col}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
