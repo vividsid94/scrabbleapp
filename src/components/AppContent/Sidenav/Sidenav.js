85a89c8f876030fb36fb891fb6778ebc974c9256
vividsid94 <siddharth.murali2008@gmail.com>
2022-12-22 00:31:37 -0600

    navbar

--- src/components/AppContent/Sidenav/Sidenav.js
+++ src/components/AppContent/Sidenav/Sidenav.js
@@ -10,6 +10,7 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 
 import { IoHome } from "react-icons/io5";
+import { IoEyeSharp } from "react-icons/io5";
 
 import styles from './Sidenav.module.css';
 const drawerWidth = 190;
@@ -31,6 +32,7 @@ const closedMixin = (theme) => ({
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
+  fontFamily: 'Josefin Sans',
   background: "#14000b",
   backgroundImage: "url('https://www.transparenttextures.com/patterns/rough-cloth.png')",
   overflowX: 'hidden',
@@ -43,6 +45,7 @@ const closedMixin = (theme) => ({
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
+  fontFamily: 'Josefin Sans',
   justifyContent: 'flex-end',
   padding: theme.spacing(0, 1),
   ...theme.mixins.toolbar,
@@ -70,7 +73,6 @@ export default function MiniDrawer() {
   const [open, setOpen] = React.useState(false);
 
   const handleDrawer = () => {
-    console.log('Drawer Toggled.');
     if (open){
       setOpen(false);
     }
@@ -87,11 +89,21 @@ export default function MiniDrawer() {
         </DrawerHeader>
         <List className={styles.btnContainer}>
           <a id="homeBtn" className={styles.link} href="/">
-            <ListItem sx={{padding: '5px', display: 'flex'}}>
+            <ListItem className={styles.listItem}>
               <ListItemIcon>
                 <IoHome className={styles.homeLogo}/>
               </ListItemIcon>
-              <ListItemText primary={"Home"} />
+              <ListItemText className={styles.listItemText} primary={"Home"} />
+            </ListItem>
+          </a>
+        </List>
+        <List className={styles.btnContainer}>
+          <a id="viewerBtn" className={styles.link} href="/viewer">
+            <ListItem className={styles.listItem}>
+              <ListItemIcon>
+                <IoEyeSharp className={styles.homeLogo}/>
+              </ListItemIcon>
+              <ListItemText className={styles.listItemText} primary={"Viewer"} />
             </ListItem>
           </a>
         </List>
