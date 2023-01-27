f1355a2f3dd40fccfccef555678cb3c2a936b749
vividsid94 <siddharth.murali2008@gmail.com>
2023-01-27 16:10:40 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [appState, setAppState] = useState('VIEWER');
   return (
     <div className="App">
-      <header className="App-header" style={{backgroundColor: appState === 'VIEWER' ? '#000003' : '#5e2700'}}>
+      <header className="App-header" style={{backgroundColor: appState === 'VIEWER' ? '#000003' : '#6E5D42'}}>
         <Router>
           <Routes>
             <Route path="/viewer" element={<Viewer onChange={setAppState}/>} />
