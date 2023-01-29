a7283f8dc58ae72c13cb7f655eeaef4d9581a229
vividsid94 <siddharth.murali2008@gmail.com>
2023-01-29 09:11:17 -0600

    footer

--- src/components/AppContent/Footer/Footer.js
+++ src/components/AppContent/Footer/Footer.js
@@ -3,7 +3,7 @@ import styles from './Footer.module.css';
 import BottomNavigation from '@mui/material/BottomNavigation';
 const Footer = () => (
   <BottomNavigation className={styles.footer}>
-    <span className={styles.footerText}>"Step up your game with every tile turn - Sid @ Tile Turnover" - ChatGPT</span>
+    <span className={styles.footerText}>"Step up your game with every tile turned - Sid @ Tile Turnover" - ChatGPT</span>
     <span className={styles.footerText}>SCRABBLE® is a registered trademark of Hasbro, Inc. in the USA and Canada.</span>
     <span className={styles.footerText}>cross-tables is copyrighted and its data is used with permission</span>
   </BottomNavigation>
