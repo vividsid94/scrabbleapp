84ece653e63e0350adc7cb1eebbadfce74b10498
vividsid94 <siddharth.murali2008@gmail.com>
2023-02-25 04:13:16 -0600

    try batch processing new tiles

--- src/components/AppContent/Board/Cell.js
+++ src/components/AppContent/Board/Cell.js
@@ -5,7 +5,7 @@ export default (function() {
     let preloadedImages = {}; // define variable in closure scope
     function preload() {
       allLetters.forEach(letter => {
-        let srcString = '/images/white-pro/' + letter + '.png';
+        let srcString = '/images/batch_processed_images/' + letter + '.png';
         preloadedImages[letter] = new Image();
         preloadedImages[letter].src = srcString;
       });
@@ -40,10 +40,10 @@ export default (function() {
     switch (type) {
         case "board":
             if (tiles === "PROTILES"){
-                return (<div className={`${styles[classZ]}`} style={{ backgroundColor: bonus.color, boxShadow: bonus.boxShadow, backgroundImage: bonus.image}}><div className={styles.decalContainer}><div className={`${styles[classN]} ${styles[classO]}`}><div className={`${styles[classQ]} ${styles[classP]}`}>{cell(bonus.value)}</div></div></div></div>);
+                return (<div className={`${styles[classZ]}`} style={{ background: bonus.color, boxShadow: bonus.boxShadow}}><div className={styles.decalContainer}><div className={`${styles[classN]} ${styles[classO]}`}><div className={`${styles[classQ]} ${styles[classP]}`}>{cell(bonus.value)}</div></div></div></div>);
             }
             else{
-                return (<div className={styles[cellTheme]} style={{ backgroundColor: bonus.color, boxShadow: bonus.boxShadow, backgroundImage: bonus.image}}>{bonus.value}</div>);
+                return (<div className={styles[cellTheme]} style={{ background: bonus.color, boxShadow: bonus.boxShadow}}>{bonus.value}</div>);
             }
         case "pool":
             return (<div className={styles.cellPool}>{bonus.value}</div>);
--- src/components/AppContent/Board/cellType.js
+++ src/components/AppContent/Board/cellType.js
@@ -9,54 +9,54 @@ export default function CellType(type, flag, border, tiles){
     let regBoxShadow = 'inset 0.15px 0.15px ' + boxShadowColor + ', inset -0.15px -0.15px ' + boxShadowColor;
     let boxShadow = 'inset 0.25px 0.25px ' + boxShadowColor + ', inset -0.25px -0.25px ' + boxShadowColor;
     if (left && top) {
-        boxShadow = 'inset 1.5px 1.5px ' + highlightColor;
+        boxShadow = 'inset 2.0px 2.0px ' + highlightColor;
     } else if (left) {
-        boxShadow = 'inset 1.5px 0px ' + highlightColor;
+        boxShadow = 'inset 2.0px 0px ' + highlightColor;
     } else if (top) {
-        boxShadow = 'inset 0px 1.5px ' + highlightColor;
+        boxShadow = 'inset 0px 2.0px ' + highlightColor;
     }
     
     if (right && bottom) {
-        boxShadow += ', inset -1.5px -1.5px ' + highlightColor;
+        boxShadow += ', inset -2.0px -2.0px ' + highlightColor;
     } else if (right) {
-        boxShadow += ', inset -1.5px 0px ' + highlightColor;
+        boxShadow += ', inset -2.0px 0px ' + highlightColor;
     } else if (bottom) {
-        boxShadow += ', inset 0px -1.5px ' + highlightColor;
+        boxShadow += ', inset 0px -2.0px ' + highlightColor;
     }
 
     switch (type) {
         case 0:
             result = {
                 "value": "",
-                "color": "papayawhip",
+                "color": "linear-gradient(to right bottom, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.9))",
                 "boxShadow": regBoxShadow
             };
             break;
         case 1:
             result = {
                 "value": "",
-                "color": "powderblue",
+                "color": "linear-gradient(to right bottom, rgba(126, 214, 221, 0.3), rgba(126, 214, 221, 0.95))",
                 "boxShadow": regBoxShadow
             };
             break;
         case 2:
             result = {
                 "value": "",
-                "color": "mediumslateblue",
+                "color": "linear-gradient(to right bottom, rgba(114, 105, 214, 0.6), rgba(114, 105, 214, 0.95))",
                 "boxShadow": regBoxShadow
             };
             break;
         case 3:
             result = {
                 "value": "",
-                "color": "lightpink",
+                "color": "linear-gradient(to right bottom, rgba(244, 159, 212, 0.5), rgba(244, 159, 212, 0.9))",
                 "boxShadow": regBoxShadow
             };
             break;
         case 4:
             result = {
                 "value": "",
-                "color": "#BE3939",
+                "color": "linear-gradient(to right bottom, rgba(206, 34, 34, 0.5), rgba(206, 34, 34, 0.9))",
                 "boxShadow": regBoxShadow
             };
             break;
