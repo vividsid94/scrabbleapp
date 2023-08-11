76796ce32cba2dcf7967ab4d19bf3461c1bd269c
vividsid94 <siddharth.murali2008@gmail.com>
2023-08-11 04:22:56 -0500

    another title change

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -15,7 +15,7 @@ export default function Home(){
           Tile Turnover<PsychologyAltIcon className={styles.questionMark}/>
         </Box>
         <Box className={styles.subTitle}>
-          Hi! I am Tile Turnover, a front-end-only project. I am undergoing off-and-on development. I will eventually be rolling out a variety of games and tools relating to Scrabble®. Stay tuned!
+          Hi! I am Tile Turnover, a front-end-only project. I am undergoing off-and-on development. Stay tuned!
         </Box>
         <Box className={styles.homeButtonContainer}>
           <Link to="/viewer">
