da036172381c92e75e98ed019326bf57773fa5a3
vividsid94 <siddharth.murali2008@gmail.com>
2025-05-01 04:02:54 -0500

    Title Styling

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidenav from '../../components/AppContent/Sidenav/Sidenav.js';
 import Box from '@mui/material/Box';
 import styles from './Home.module.css';
 import PsychologyAltIcon from '@mui/icons-material/PsychologyAlt';
 import { Link } from 'react-router-dom';
+import ExtensionIcon from '@mui/icons-material/Extension';
 
 export default function Home(){
-  
+  const [isSpinning, setIsSpinning] = useState(false);
+
+  const handleIconClick = () => {
+    setIsSpinning(true);
+    setTimeout(() => setIsSpinning(false), 500);
+  };
+
   return (
     <Box sx={{ display: 'flex'}}>
       <Sidenav/>
       <Box className={styles.page}>
         <Box className={styles.title}>
-          Tile Turnover<PsychologyAltIcon className={styles.questionMark}/>
-        </Box>  
+          Tile Turnover
+          <ExtensionIcon 
+            className={`${styles.titleIcon} ${isSpinning ? styles.spinning : ''}`}
+            onClick={handleIconClick}
+            style={{ cursor: 'pointer' }}
+          />
+        </Box>
         <Box className={styles.developmentMessage}>
           Hi! I'm Tile Turnover, a front-end focused project. Development has restarted! Check out the <Link to="/changelog" style={{color: '#3D5A80', textDecoration: 'none', fontWeight: 'bold'}}>changelog</Link> for updates! 🚀
         </Box>
