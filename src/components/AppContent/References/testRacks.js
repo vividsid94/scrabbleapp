0012f764ab3b5e95a7bc9250ca79b7d33d09cdc8
vividsid94 <siddharth.murali2008@gmail.com>
2025-06-05 12:07:37 -0500

    Update testRacks.js

--- src/components/AppContent/References/testRacks.js
+++ src/components/AppContent/References/testRacks.js
@@ -1,7 +1,7 @@
 // Test rack configurations
 // Set to null to use random racks
 export const TEST_RACKS = {
-  enabled: true, // Set to false to use random racks
+  enabled: false, // Set to false to use random racks
   player1: ['E', 'C', 'C', 'D', 'L', 'F', 'G'], // Example test rack
-  player2: ['H', 'I', 'J', 'K', 'L', 'M', 'N']  // Example test rack
+  player2: ['E', 'V', 'V', 'U', 'U', 'L', 'U']  // Example test rack
 }; 
