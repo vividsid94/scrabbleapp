4b9371a71084f9105dba7b602fb2941e1d009b0d
vividsid94 <siddharth.murali2008@gmail.com>
2025-06-14 11:27:25 -0500

    Update rackFunctions.js

--- src/functions/rackFunctions.js
+++ src/functions/rackFunctions.js
@@ -3,6 +3,6 @@ import Cell from "../components/AppContent/Board/Cell";
 export const createRack = (moveSet, currentMoveRef) => {
     var move = moveSet ? moveSet[currentMoveRef + 1] : null;
     const parts = move ? move.split(" ") : '';
-    const rack = parts ? parts[1].replace(/\?/g, " ").split('') : [];
+    const rack = parts ? parts[1].split('') : [];
     return rack;
 }
