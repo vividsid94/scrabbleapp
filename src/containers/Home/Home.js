86edcc099d6a9e83db82b622ffc067b0a35e2819
vividsid94 <siddharth.murali2008@gmail.com>
2025-07-17 15:12:55 -0500

    home update

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,50 +4,28 @@ import Box from '@mui/material/Box';
 import styles from './Home.module.css';
 import PsychologyAltIcon from '@mui/icons-material/PsychologyAlt';
 import { Link } from 'react-router-dom';
-import ExtensionIcon from '@mui/icons-material/Extension';
 
 export default function Home(){
-  const [isSpinning, setIsSpinning] = useState(false);
-  const [showLinks, setShowLinks] = useState(false);
-
-  const devMessage = "Hi! I'm Tile Turnover, a front-end focused project. Development has restarted! Check out the changelog for updates! 🚀";
-
-  const handleIconClick = () => {
-    setIsSpinning(true);
-    setShowLinks(prev => !prev);
-    setTimeout(() => setIsSpinning(false), 500);
-  };
+  const devMessage = "Hi! I'm Tile Turnover™, a front-end focused project. Development has restarted! Check out the changelog for updates! New features will gradually appear as buttons here. 🚀";
 
   return (
     <Box sx={{ display: 'flex'}}>
       <Sidenav/>
       <Box className={styles.page}>
         <Box className={styles.title}>
-          Tile Turnover
-          <ExtensionIcon 
-            className={`${styles.titleIcon} ${isSpinning ? styles.spinning : ''}`}
-            onClick={handleIconClick}
-            style={{ cursor: 'pointer' }}
-          />
+          Tile Turnover™
         </Box>
         <Box className={styles.developmentMessage}>
-          {showLinks ? (
-            <div className={styles.scrabbleLinks}>
-              <a href="https://www.cross-tables.com/" target="_blank" rel="noopener noreferrer" className={styles.link}>Cross-Tables 🏆</a>
-              <a href="https://www.youtube.com/@MackMeller" target="_blank" rel="noopener noreferrer" className={styles.link}>Mack Meller's Channel 🎥</a>
-              <a href="https://randomracer.com/" target="_blank" rel="noopener noreferrer" className={styles.link}>Random Racer 🎮</a>
-            </div>
-          ) : (
-            <>
-              {devMessage.split("changelog")[0]}
-              <Link to="/changelog" style={{color: '#3D5A80', textDecoration: 'none', fontWeight: 'bold'}}>changelog</Link>
-              {devMessage.split("changelog")[1]}
-            </>
-          )}
+          {devMessage.split("changelog")[0]}
+          <Link to="/changelog" style={{color: '#3D5A80', textDecoration: 'none', fontWeight: 'bold'}}>changelog</Link>
+          {devMessage.split("changelog")[1]}
         </Box>
         <Box className={styles.homeButtonContainer}>
           <Link to="/viewer">
             <button className={styles.homeButton}>Annotated Game Viewer</button>
+          </Link>
+          <Link to="/submit-game">
+            <button className={styles.submitGameButton}>Submit Game</button>
           </Link>
           {/* <Link to="/play">
             <button className={styles.homeButton}>Play Scrabble</button>
